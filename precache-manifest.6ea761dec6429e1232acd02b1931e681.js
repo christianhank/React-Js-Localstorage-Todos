f19b9b20365542ae8f34203ed35424888597b49f
Christian Hank <christian.hank93@gmail.com>
2022-05-25 19:26:44 +0200

    Updates

---
+++ precache-manifest.6ea761dec6429e1232acd02b1931e681.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770e07d58d1aa4001745f707c5630559",
+    "url": "/React-Js-Localstorage-Todos/index.html"
+  },
+  {
+    "revision": "39c49b3e3cb8bfcb28b1",
+    "url": "/React-Js-Localstorage-Todos/static/css/main.5859f434.chunk.css"
+  },
+  {
+    "revision": "7d407f4dbbdaaf56b3d7",
+    "url": "/React-Js-Localstorage-Todos/static/js/2.3399f4b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Js-Localstorage-Todos/static/js/2.3399f4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c49b3e3cb8bfcb28b1",
+    "url": "/React-Js-Localstorage-Todos/static/js/main.e7e5245f.chunk.js"
+  },
+  {
+    "revision": "ef276471600ba54ff952",
+    "url": "/React-Js-Localstorage-Todos/static/js/runtime-main.e38283bb.js"
+  },
+  {
+    "revision": "73800790498686501ca9517428d713f8",
+    "url": "/React-Js-Localstorage-Todos/static/media/bg.73800790.jpg"
+  },
+  {
+    "revision": "837c7aa47227f88ee37020ccb184243b",
+    "url": "/React-Js-Localstorage-Todos/static/media/loeschen.837c7aa4.png"
+  },
+  {
+    "revision": "e418339f79b959e72da46cbdb4d6271e",
+    "url": "/React-Js-Localstorage-Todos/static/media/pruefen.e418339f.png"
+  }
+]);
